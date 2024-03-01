6a813f4c4a1cb1741f15bd72c21f035ce9aae900
Lucas Andrade <contato.developerlucas@gmail.com>
2024-02-29 23:56:15 -0300

    feat: add the finished sidebar to the main page

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,53 @@
+import * as Input from '../input'
+import { NavItem } from './NavItem'
+import { Profile } from './Profile'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Logo } from './logo'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+
+export function Sidebar() {
+  return (
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-500" />
+
+        <Profile />
+      </div>
+    </aside>
+  )
+}
